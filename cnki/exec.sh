826599f8b3c88bbd58cc44e988093f635cc11338
qiangxu <toxuqiang@gmail.com>
2025-03-30 15:59:39 +0800

    feature: cleanup overload after 10 times

--- cnki/exec.sh
+++ cnki/exec.sh
@@ -3,9 +3,12 @@
 # CREATED DATE: Sun Mar 30 02:04:01 2025
 # CREATED BY: qiangxu, toxuqiang@gmail.com
 ##############################################################
-
-for i in {1..5000}; do
-    rm site3/links/cnki_*; python site3/search_site_3.py -c config/3.json -S -s 3 -t 1 -z 20
-    python site3/dump_site_3.py -c config/3.json
-    python site3/dump_site_3.py -c config/3.json
+for j in {1..100}; do 
+    rm site3/links/*.json
+    rm site3/state.json
+    for i in {1..10}; do
+        python site3/search_site_3.py -c config/3.json -S -s 3 -t 1 -z 20
+        python site3/dump_site_3.py -c config/3.json
+        python site3/dump_site_3.py -c config/3.json
+    done
 done
